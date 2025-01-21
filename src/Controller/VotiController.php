f2befae09b368c71780abf7e6237ff97e52c3584
Jason-unicam <jason.bini@studenti.unicam.it>
2025-01-21 18:43:57 +0100

    + Grafico funzionante

--- src/Controller/VotiController.php
+++ src/Controller/VotiController.php
@@ -6,13 +6,79 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use Symfony\UX\Chartjs\Model\Chart;
+
 class VotiController extends AbstractController
 {
     #[Route('/voti', name: 'app_voti')]
-    public function index(): Response
+    public function index(ChartBuilderInterface $chartBuilder): Response
     {
+        $chart = $chartBuilder->createChart(Chart::TYPE_LINE);
+
+        $chart->setData([
+            'labels' => ['Gen', 'Feb', 'Mar', 'Apr', 'Mag', 'Giu', 'Lug', 'Ago', 'Set', 'Ott', 'Nov', 'Dic'],
+            'datasets' => [
+                [
+                    'backgroundColor' => 'rgba(52, 105, 223, 0.2)', // Area sotto la linea
+                    'borderColor' => '#3469DF', // Colore della linea
+                    'data' => [18, 22, 25, 24, 28, 30, 31, 20, 22, 25, 24, 28, 30, 31], // Dati compresi tra 18 e 31
+                    'fill' => true, // Riempi sotto la linea
+                ],
+            ],
+        ]);
+
+        $chart->setOptions([
+            'plugins' => [
+                'legend' => [
+                    'display' => false, // Nasconde la label e il rettangolino
+                ],
+            ],
+            'scales' => [
+                'x' => [
+                    'grid' => [
+                        'display' => false, // Nasconde le linee verticali
+                        
+                    ],
+                    'ticks' => [
+                        'color' => '#D3D5EE', // Colore delle etichette
+                        'font' => [
+                            'size' => 12,
+                        ],
+                    ],
+                ],
+                'y' => [
+                    'grid' => [
+                        'color' => '#ECF4FF', // Colore delle linee orizzontali
+                        'borderDash' => [], // Linea continua
+                    ],
+                    'ticks' => [
+                        'stepSize' => 1, // Intervallo dei tick
+                        'color' => '#3469DF', // Colore delle etichette
+                        'font' => [
+                            'size' => 12,
+                        ],
+                    ],
+                    'min' => 18, // Valore minimo
+                    'max' => 32, // Valore massimo (oltre 31 per includere l'ultimo tick)
+                ],
+            ],
+            'elements' => [
+                'line' => [
+                    'tension' => 0.4, // Linea curva
+                    'borderWidth' => 4, // Spessore della linea
+                ],
+                'point' => [
+                    'radius' => 0, // Rimuove i punti
+                ],
+            ],
+            'responsive' => true,
+            'maintainAspectRatio' => false, // Permette dimensioni personalizzate
+        ]);
+        
+
         return $this->render('pages/voti/index.html.twig', [
-            'controller_name' => 'VotiController',
+            'chart' => $chart,
         ]);
     }
 }
