e561eaeb25a505ba3fbc3a0bec89299875e12eb1
maxim-cognigni <115725797+maxim-cognigni@users.noreply.github.com>
2025-02-03 03:20:44 +0100

    + Aggiunto Endpoint per aggiungere corsi (TODO: Da migliorare il controllo dell'input e il codice in generale)

--- src/Controller/CorsiController.php
+++ src/Controller/CorsiController.php
@@ -3,12 +3,18 @@
 namespace App\Controller;
 
 use App\Entity\Corso;
+use App\Entity\Studente;
+use App\Entity\Utente;
+use App\HttpUtils\HttpError;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Routing\Requirement\Requirement;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 #[Route('/corsi')]
 class CorsiController extends AbstractController
@@ -35,11 +41,8 @@ class CorsiController extends AbstractController
     #[Route('/get/{id}', name: 'api_get_corso', requirements: ['id' => Requirement::DIGITS])]
     public function getCorso(int $id): JsonResponse
     {
-        if (!$this->isGranted('IS_AUTHENTICATED')) return new JsonResponse([
-            'status' => '401',
-            'error' => 'Unauthorized',
-            'message' => '',
-        ]);
+        if (!$this->isGranted('IS_AUTHENTICATED')) return new JsonResponse(HttpError::UNAUTHORIZED->getJsonMessage());
+
         $corso = $this->entityManager->getRepository(Corso::class)->getCorsoById($id);
 
         return new JsonResponse([
@@ -53,9 +56,44 @@ class CorsiController extends AbstractController
         ]);
     }
 
-    #[Route('/add', name: 'app_add_corso')]
-    public function addCorso(string $id): string
+    #[Route('/add', name: 'api_add_corso')]
+    public function addCorso(
+        #[MapQueryParameter] string $nome = '',
+        #[MapQueryParameter('anno_svolgimento')] string $annoSvolgimento = '',
+        #[MapQueryParameter] string $codice = '',
+        #[MapQueryParameter] string $docente = '',
+        #[MapQueryParameter] int $cfu = 0,
+        #[MapQueryParameter] string $note = ''
+
+    ): JsonResponse
     {
-        return "";
+        if (!$this->isGranted('IS_AUTHENTICATED')) return new JsonResponse(HttpError::UNAUTHORIZED->getJsonMessage());
+
+        $nome = strip_tags($nome);
+        $annoSvolgimento = strip_tags($annoSvolgimento);
+        $codice = strip_tags($codice);
+        $docente = strip_tags($docente);
+        $note = strip_tags($note);
+
+        $corso = new Corso();
+        $corso
+            ->setNome($nome)
+            ->setAnnoSvolgimento($annoSvolgimento)
+            ->setCodice($codice)
+            ->setDocente($docente)
+            ->setCfu($cfu)
+            ->setNote($note);
+
+        $studente = $this->entityManager->getRepository(Utente::class)->getUtenteByUsername($this->getUser()->getUserIdentifier())->getStudente();
+        $studente->addcorso($corso);
+        $this->entityManager->persist($corso);
+        $this->entityManager->persist($studente);
+        $this->entityManager->flush();
+
+        return new JsonResponse([
+            'status' => '200',
+            'message' => 'Corso aggiunto',
+            'id' => $corso->getId(),
+        ]);
     }
 }
