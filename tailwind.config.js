63775e868682008723eceef5222390ffea60ccb7
Jason-unicam <jason.bini@studenti.unicam.it>
2025-02-13 20:25:11 +0100

    + Aggiunti modal di corsi e esami, fatto anche il login e il register, che sono da rivedere

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,8 @@ module.exports = {
   content: [
     "./assets/**/*.js",
     "./templates/**/*.html.twig",
+    "./templates/**/**/*.html.twig",
+    "./templates/pages/login/index.twig.html"
   ],
   theme: {
     extend: {},
